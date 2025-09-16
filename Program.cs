d665b33e9a8b2eafd64538e41a14b525084fc47f
339780520 <339780520@2260I-L29-008>
2025-09-16 08:48:06 -0400

    finished random integers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random generator = new Random();
+
+            //Variables
+            int min, max, die1, die2;
+
+
+
+            //Random Integers
+            Console.WriteLine("Please enter a minimum value");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Please enter a maximum value");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 5; i++)
+            {
+                int randomInt = generator.Next(min, max + 1);
+                Console.WriteLine($"Random Integer {i + 1}: {randomInt}");
+            }
         }
     }
 }
